7f4c8971b7086e5f020f4f75b021123e823e11ac
Njabulo Hope Makana <makanahopenjabulo@gmail.com>
2026-07-16 21:32:09 +0200

    Added challenge

---
+++ Unit1/TheConcertTicketBooker.py
@@ -0,0 +1,8 @@
+#User's name
+user_name = (input("Please enter your name: "))
+
+#Name of artist the user will enter
+artist_name = (input("Please enter the name of an artist: "))
+
+#f-string to print confirmation message
+print(f"Hey {user_name}! Your tickets to see {artist_name} have been booked successfully!")
